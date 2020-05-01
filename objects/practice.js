35677a04bf7c0375fbf3e58c552c54116ce8dc9b
caden-knight <cadenknight@gmail.com>
2020-04-30 23:27:42 -0600

    day-4-solutions

--- objects/practice.js
+++ objects/practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+let first = (arr) => {
+  return arr[0]
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+let last = (arr) => {
+  return arr[arr.length - 1]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+let looper = (family) => {
+  for (let i = 0; i < family.length; i++) {
+    alert(family[i])
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+let reversedLooper = (letters) => {
+  for (let i = letters.length - 1; i >= 0; i-- ) {
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,9 +90,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+let evenFinder = (arr) => {
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (arr[i] % 2 !== 0) {
+      arr.splice(i, 1)
+    }
+  } return arr
+}
 
-
-
+console.log(evenFinder(nums))
 
 
 
